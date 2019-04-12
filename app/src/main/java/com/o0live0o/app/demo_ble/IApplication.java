9462ca89aa7318625b738d6bdd1e2dc5b62c080c
yizha <443342311@qq.com>
2019-04-12 21:53:38 +0800

    创建入口文件

---
+++ app/src/main/java/com/o0live0o/app/demo_ble/IApplication.java
@@ -0,0 +1,11 @@
+package com.o0live0o.app.demo_ble;
+
+import android.app.Application;
+
+public class IApplication extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+}
